2d6dbba207676013932db5cb63e56a8e3c790553
Damir Foy <damirfoy@gmail.com>
2013-03-24 03:47:30 +0400

    update

--- src/unslider.js
+++ src/unslider.js
@@ -1,26 +1,26 @@
 /**
  *   Unslider by @idiot
  */
- 
+
 (function($, f) {
 	//  If there's no jQuery, Unslider can't work, so kill the operation.
 	if(!$) return f;
-	
+
 	var Unslider = function() {
 		//  Set up our elements
 		this.el = f;
 		this.items = f;
-		
+
 		//  Dimensions
 		this.sizes = [];
 		this.max = [0,0];
-		
+
 		//  Current inded
 		this.current = 0;
-		
+
 		//  Start/stop timer
 		this.interval = f;
-				
+
 		//  Set some options
 		this.opts = {
 			speed: 500,
@@ -28,122 +28,168 @@
 			complete: f, // when a slide's finished
 			keys: !f, // keyboard shortcuts - disable if it breaks things
 			dots: f, // display ••••o• pagination
-			fluid: f, // is it a percentage width?,
+			fluid: f // is it a percentage width?,
 		};
-		
+
 		//  Create a deep clone for methods where context changes
 		var _ = this;
+
+		// Check for CSS3 support
+		var css3 = (function(doc) {
+			var prop = {
+				OTransition: ['oTransitionEnd otransitionend', 'OTransform'],
+				msTransition: ['transitionend', 'msTransform'],
+				MozTransition: ['transitionend', 'MozTransform'],
+				WebkitTransition: ['webkitTransitionEnd', 'webkitTransform'],
+				transition: ['transitionend', 'transform']
+			};
+
+			for (each in prop)
+				if (typeof doc.style[each] == 'string') return {name: each, callback: prop[each][0], accel: prop[each][1]};
+
+			return f;
+		})(document.body || document.documentElement);
 
 		this.init = function(el, opts) {
 			this.el = el;
 			this.ul = el.children('ul');
-			this.max = [el.outerWidth(), el.outerHeight()];			
+			this.max = [el.outerWidth(), el.outerHeight()];
 			this.items = this.ul.children('li').each(this.calculate);
-			
+
 			//  Check whether we're passing any options in to Unslider
 			this.opts = $.extend(this.opts, opts);
-			
+
 			//  Set up the Unslider
 			this.setup();
-			
+
 			return this;
 		};
-		
+
 		//  Get the width for an element
 		//  Pass a jQuery element as the context with .call(), and the index as a parameter: Unslider.calculate.call($('li:first'), 0)
 		this.calculate = function(index) {
 			var me = $(this),
 				width = me.outerWidth(), height = me.outerHeight();
-			
+
 			//  Add it to the sizes list
 			_.sizes[index] = [width, height];
-			
+
 			//  Set the max values
 			if(width > _.max[0]) _.max[0] = width;
 			if(height > _.max[1]) _.max[1] = height;
 		};
-		
+
 		//  Work out what methods need calling
 		this.setup = function() {
+			var elCSS = {width: _.max[0], height: this.items.first().outerHeight(), overflow: 'hidden'},
+				ulCSS = {position: 'relative', left: 0, width: (this.items.length * 100) + '%'};
+
+			if (css3) {
+				elCSS[css3.accel] = 'translateZ(0)';
+				ulCSS[css3.accel] = 'translateZ(0)';
+			}
+
 			//  Set the main element
-			this.el.css({
-				overflow: 'hidden',
-				width: _.max[0],
-				height: this.items.first().outerHeight()
-			});
-			
+			this.el.css(elCSS);
+
 			//  Set the relative widths
-			this.ul.css({width: (this.items.length * 100) + '%', position: 'relative'});
+			this.ul.css(ulCSS);
 			this.items.css('width', (100 / this.items.length) + '%');
-			
+
 			if(this.opts.delay !== f) {
 				this.start();
 				this.el.hover(this.stop, this.start);
 			}
-			
+
 			//  Custom keyboard support
 			this.opts.keys && $(document).keydown(this.keys);
-			
+
 			//  Dot pagination
 			this.opts.dots && this.dots();
-			
+
 			//  Little patch for fluid-width sliders. Screw those guys.
 			if(this.opts.fluid) {
 				var resize = function() {
 					_.el.css('width', Math.min(Math.round((_.el.outerWidth() / _.el.parent().outerWidth()) * 100), 100) + '%');
 				};
-				
+
 				resize();
 				$(window).resize(resize);
 			}
-			
+
 			if(this.opts.arrows) {
-				this.el.parent().append('<p class="arrows"><span class="prev">←</span><span class="next">→</span></p>')
-					.find('.arrows span').click(function() {
+				this.el.parent().append('<p class="arrows"><span class="prev">←</span><span class="next">→</span></p>').find('.arrows span').click(function() {
 						$.isFunction(_[this.className]) && _[this.className]();
 					});
 			};
-			
+
 			//  Swipe support
 			if($.event.swipe) {
 				this.el.on('swipeleft', _.prev).on('swiperight', _.next);
 			}
 		};
-		
+
 		//  Move Unslider to a slide index
 		this.move = function(index, cb) {
 			//  If it's out of bounds, go to the first slide
-			if(!this.items.eq(index).length) index = 0;
-			if(index < 0) index = (this.items.length - 1);
-			
-			var target = this.items.eq(index);
-			var obj = {height: target.outerHeight()};
-			var speed = cb ? 5 : this.opts.speed;
-			
-			if(!this.ul.is(':animated')) {			
-				//  Handle those pesky dots
-				_.el.find('.dot:eq(' + index + ')').addClass('active').siblings().removeClass('active');
+			if (!this.items.eq(index).length) index = 0;
+			if (index < 0) index = (this.items.length - 1);
 
-				this.el.animate(obj, speed) && this.ul.animate($.extend({left: '-' + index + '00%'}, obj), speed, function(data) {
-					_.current = index;
-					$.isFunction(_.opts.complete) && !cb && _.opts.complete(_.el);
+			var target = this.items.eq(index),
+				animH = target.outerHeight() + 'px',
+				animL = '-' + index + '00%',
+				anim = {height: animH},
+				speed = cb ? 5 : this.opts.speed,
+				speedT = speed + 'ms ease-in-out',
+				styl = {el: this.el[0].style, ul: this.ul[0].style};
+
+			if (css3) {
+				var anim = 'height ' + speedT;
+
+				_.moveBefore(index);
+				styl.el[css3.name] = anim;
+				styl.el['height'] = animH;
+				styl.ul[css3.name] = anim + ', left ' + speedT;
+				styl.ul['left'] = animL;
+
+				this.ul.bind(css3.callback, function() {
+					$(this).unbind(css3.callback);
+					_.moveAfter(index);
 				});
+			} else {
+				if (!this.ul.is(':animated')) {
+					_.moveBefore(index);
+
+					this.el.animate(anim, speed);
+					this.ul.animate($.extend({left: animL}, anim), speed, function() {
+						_.moveAfter(index);
+					});
+				}
 			}
 		};
-		
+
+		this.moveBefore = function(index) {
+			_.el.find('.dot:eq(' + index + ')').addClass('active').siblings().removeClass('active');
+		};
+
+		this.moveAfter = function(index) {
+			_.current = index;
+			$.isFunction(_.opts.complete) && !cb && _.opts.complete(_.el);
+		};
+
 		//  Autoplay functionality
 		this.start = function() {
 			_.interval = setInterval(function() {
 				_.move(_.current + 1);
 			}, _.opts.delay);
 		};
-		
+
 		//  Stop autoplay
 		this.stop = function() {
 			_.interval = clearInterval(_.interval);
 			return _;
 		};
-		
+
 		//  Keypresses
 		this.keys = function(e) {
 			var key = e.which;
@@ -151,43 +197,43 @@
 				//  Prev/next
 				37: _.prev,
 				39: _.next,
-				
+
 				//  Esc
 				27: _.stop
 			};
-			
+
 			if($.isFunction(map[key])) {
 				map[key]();
 			}
 		};
-		
+
 		//  Arrow navigation
 		this.next = function() { return _.stop().move(_.current + 1) };
 		this.prev = function() { return _.stop().move(_.current - 1) };
-		
+
 		this.dots = function() {
 			//  Create the HTML
 			var html = '<ol class="dots">';
 				$.each(this.items, function(index) { html += '<li class="dot' + (index < 1 ? ' active' : '') + '">' + (index + 1) + '</li>'; });
 				html += '</ol>';
-			
+
 			//  Add it to the Unslider
 			this.el.addClass('has-dots').append(html).find('.dot').click(function() {
-				_.move($(this).index());
+				_.stop().move($(this).index());
 			});
 		};
 	};
-	
+
 	//  Create a jQuery plugin
 	$.fn.unslider = function(o) {
 		var len = this.length;
-		
+
 		//  Enable multiple-slider support
 		return this.each(function(index) {
-			//  Cache a copy of $(this), so it 
+			//  Cache a copy of $(this), so it
 			var me = $(this);
 			var instance = (new Unslider).init(me, o);
-			
+
 			//  Invoke an Unslider instance
 			me.data('unslider' + (len > 1 ? '-' + (index + 1) : ''), instance);
 		});
