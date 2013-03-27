3eb2f13e4090edab1cc16d59e0a66109f169ba66
Damir Foy <damirfoy@gmail.com>
2013-03-27 14:31:19 +0400

    Optimization
    Optimized a bit.
    -1.6 kb of regular and -0.5 of minified size.

--- src/unslider.js
+++ src/unslider.js
@@ -4,7 +4,7 @@
 
 (function($, f, doc, _transitionend, _TransitionEnd) {
 	var Unslider = function() {
-		//  Cache strings
+		//  Cached strings
 		var _special = $.event.special,
 			_move = 'move',
 			_movestart = _move + 'start',
@@ -15,11 +15,14 @@
 			_mouseout = 'mouseout',
 			_dot = 'dot';
 
+		//  Clone attack
+		var _ = this;
+
 		//  Current inded
-		this.i = 0;
+		_.i = 0;
 
 		//  Set some options
-		this.o = {
+		_.o = {
 			speed: 500,  // animation speed, false for no transition (integer)
 			delay: 3000, // delay between slides, false for no autoplay (integer or boolean)
 			init: 0,     // init delay, false for no delay (integer or boolean)
@@ -34,20 +37,18 @@
 			touch: _drag // or 'swipe', false to disable (string or boolean)
 		};
 
-		//  Create a deep clone for methods where context changes
-		var _ = this;
-
-		//  Detect css3 support
+		//  Detect CSS3 support
 		var css3,
 			props = {OTransition: 'o' + _TransitionEnd + ' o' + _transitionend, msTransition: _transitionend, MozTransition: _transitionend, WebkitTransition: 'webkit' + _TransitionEnd, transition: _transitionend};
-		for (prop in props)
-			if (typeof doc.documentElement.style[prop] == 'string') css3 = [prop, props[prop]];
 
-		this.init = function(el, o) {
-			this.el = el;
-			this.ul = el.find('>ul');
-			this.max = [el.outerWidth() | 0, el.outerHeight() | 0];
-			this.li = this.ul.find('>li').each(function(index) {
+		for (prop in props)
+			if (typeof doc.documentElement.style[prop] == 'string') css3 = {n: prop, c: props[prop]};
+
+		_.init = function(el, o) {
+			_.el = el;
+			_.ul = el.find('>ul');
+			_.max = [el.outerWidth() | 0, el.outerHeight() | 0];
+			_.li = _.ul.find('>li').each(function(index) {
 				var me = $(this),
 					width = me.outerWidth(),
 					height = me.outerHeight();
@@ -58,81 +59,73 @@
 			});
 
 			//  Check whether we're passing any options in to Unslider
-			this.o = $.extend(this.o, o);
+			_.o = $.extend(_.o, o);
 
-			//  Set up the Unslider
-			this.setup();
-
-			return this;
-		};
-
-		//  Work out what methods need calling
-		this.setup = function() {
-			var o = this.o,
+			//  Cache vars
+			var o = _.o,
+				ul = _.ul,
+				li = _.li,
 				t = o.touch,
-				el = this.el,
-				ul = this.ul,
-				li = this.li,
 				len = li.length;
 
 			//  Set the main element
 			el.css({width: _.max[0], height: li.first().outerHeight(), overflow: 'hidden'});
 
 			//  Set the relative widths
-			ul.css({position: 'relative', width: (len * 100) + '%'});
+			ul.css({position: 'relative', left: 0, width: (len * 100) + '%'});
 			li.css({'float': 'left', width: (100 / len) + '%'});
 
 			//  Autoslide
-			setTimeout($.proxy(this.auto, this), o.init | 0);
+			setTimeout(_.auto, o.init | 0);
 
 			//  Keypresses
 			if (o.keys) {
-				$(doc).on('keydown', $.proxy(function(e) {
+				$(doc).keydown(function(e) {
 					var key = e.which;
 
 					if (key == 37)
-						this.prev(); // Left
+						_.prev(); // Left
 					else if (key == 39)
-						this.next(); // Right
+						_.next(); // Right
 					else if (key == 27)
-						this.stop(); // Esc
-				}, this));
+						_.stop(); // Esc
+				});
 			};
 
 			//  Dot pagination
-			o.dots && this.nav(_dot);
+			o.dots && _.nav(_dot);
 
 			//  Arrows support
-			o.arrows && this.nav('arrow');
+			o.arrows && _.nav('arrow');
 
 			//  Patch for fluid-width sliders. Screw those guys.
 			if (o.fluid) {
-				$(window).resize($.proxy(function() {
-					this.rs && clearTimeout(this.rs);
+				$(window).resize(function() {
+					_.r && clearTimeout(_.r);
 
-					this.rs = setTimeout($.proxy(function() {
-						var styl = {height: this.li.eq(this.i).outerHeight()},
-							width = this.el.outerWidth();
+					_.r = setTimeout(function() {
+						var styl = {height: li.eq(_.i).outerHeight()},
+							width = el.outerWidth();
 
-						this.max[0] = width;
-						this.ul.css(styl);
-						styl['width'] = Math.min(Math.round((width / this.el.parent().outerWidth()) * 100), 100) + '%';
-						this.el.css(styl);
-					}, this), 50);
-				}, this)).resize();
-			}
+						_.max[0] = width;
+						ul.css(styl);
+						styl['width'] = Math.min(Math.round((width / el.parent().outerWidth()) * 100), 100) + '%';
+						el.css(styl);
+					}, 50);
+				}).resize();
+			};
 
 			//  Touch support
 			if (t) {
-				this.el.on(_movestart + ' move ' + _moveend + ' ' + _swipeleft + ' swiperight', $.proxy(function(e) {
+				el.on(_movestart + ' move ' + _moveend + ' ' + _swipeleft + ' swiperight', function(e) {
 					var type = e.type,
 						x = e.distX,
 						y = e.distY,
-						index = this.i,
-						width = this.max[0],
+						index = _.i,
+						width = _.max[0],
 						left = -index * 100,
 						path = 100 * x / width,
-						styl = this.ul[0].style;
+						styl = ul[0].style;
 
 					if (t == _drag && _special[_move]) {
 						//  Drag support
@@ -141,8 +134,8 @@
 								e.preventDefault();
 								return;
 							} else {
-								if (css3) styl[css3[0]] = 'left 0ms ease';
-								this.drag = !f;
+								if (css3) styl[css3.n] = 'left 0ms ease';
+								_.drag = !f;
 							};
 						} else if (type == _move) {
 							styl['left'] = left + path + '%';
@@ -150,122 +143,122 @@
 							path = path | 0;
 
 							if (path < -10)
-								this.to(++index);
+								_.to(++index);
 							else if (path > 10)
-								this.to(--index);
+								_.to(--index);
 							else
-								this.to(index);
+								_.to(index);
 
-							this.stop();
-							this.drag = f;
+							_.stop();
+							_.drag = f;
 						};
 					} else if (t == _swipe && _special[_swipe]) {
 						//  Swipe support
-						type == _swipeleft ? this.prev() : this.next();
+						type == _swipeleft ? _.prev() : _.next();
 					};
-				}, this));
+				});
 			};
+
+			return _;
 		};
 
-		//  Autoslide it
-		this.auto = function() {
-			if (this.o.delay | 0) {
-				this.play();
+		//  Work out what methods need calling
 
-				if (this.o.pause) {
-					this.el.on('mouseover ' + _mouseout, $.proxy(function(e) {
-						this.stop();
-						e.type == _mouseout && this.play();
-					}, this));
+		//  Autoslide it
+		_.auto = function() {
+			if (_.o.delay | 0) {
+				_.play();
+
+				if (_.o.pause) {
+					_.el.on('mouseover ' + _mouseout, function(e) {
+						_.stop();
+						e.type == _mouseout && _.play();
+					});
 				};
 			};
 		};
 
 		//  Move Unslider to a slide index
-		this.to = function(index) {
-			var o = this.o,
-				cur = this.i,
-				drag = this.drag,
-				el = this.el,
-				ul = this.ul,
-				li = this.li,
-				target = li.eq(index);
+		_.to = function(index) {
+			var cur = _.i,
+				drag = _.drag,
+				target = _.li.eq(index);
 
 			//  Slide?
-			if (index == cur && !drag || (!target.length || index < 0) && o.loop == f) return;
+			if (index == cur && !drag || (!target.length || index < 0) && _.o.loop == f) return;
 
 			//  Check if it's out of bounds
 			if (!target.length) index = drag ? cur : 0;
-			if (index < 0) index = drag ? cur : li.length - 1;
-			if (index == cur) target = li.eq(cur);
+			if (index < 0) index = drag ? cur : _.li.length - 1;
+			if (index == cur) target = _.li.eq(cur);
 
-			this.i = index;
-			el.find('.dot:eq(' + index + ')').addClass('active').siblings().removeClass('active');
-			this.before(o.before);
+			_.i = index;
+			_.el.find('.dot:eq(' + index + ')').addClass('active').siblings().removeClass('active');
+			_.before(_.o.before);
 
-			var speed = o.speed | 0,
+			var speed = _.o.speed | 0,
 				tail = css3 ? speed + 'ms ease-in-out' : {duration: speed, queue: f},
 				animH = target.outerHeight() + 'px',
 				animL = '-' + index + '00%',
 				anim = css3 ? 'height ' + tail : {height: animH},
-				styl = {el: el[0].style, ul: ul[0].style};
+				styl = {el: _.el[0].style, ul: _.ul[0].style};
 
 			if (css3) {
 				//  CSS3 transition
-				styl.el[css3[0]] = anim;
+				styl.el[css3.n] = anim;
 				styl.el['height'] = animH;
-				styl.ul[css3[0]] = anim + ', left ' + tail;
+				styl.ul[css3.n] = anim + ', left ' + tail;
 				styl.ul['height'] = animH;
 				styl.ul['left'] = animL;
 
-				this.ul.on(css3[1], $.proxy(function() {
-					this.ul.off(css3[1]);
-					this.after();
-				}, this));
+				_.ul.on(css3.c, function() {
+					_.ul.off(css3.c);
+					_.after();
+				});
 			} else {
 				//  jQuery animation
-				el.animate(anim, tail);
+				_.el.animate(anim, tail);
 
 				anim['left'] = animL;
-				tail['complete'] = $.proxy(this.after, this);
-				ul.animate(anim, tail);
+				tail['complete'] = _.after;
+				_.ul.animate(anim, tail);
 			};
 		};
 
 		//  Before/after callbacks
-		this.before = function(before) {
-			$.isFunction(before) && before(this.el);
+		_.before = function(before) {
+			$.isFunction(before) && before(_.el);
 		};
 
-		this.after = function() {
-			$.isFunction(this.o.after) && this.o.after(this.el);
+		_.after = function() {
+			$.isFunction(_.o.after) && _.o.after(_.el);
 		};
 
 		//  Autoplay functionality
-		this.play = function() {
-			if (!this.drag) {
-				this.timer = setInterval($.proxy(function() {
-					this.to(this.i + 1);
-				}, this), this.o.delay | 0);
+		_.play = function() {
+			if (!_.drag) {
+				_.timer = setInterval(function() {
+					_.to(_.i + 1);
+				}, _.o.delay | 0);
 			}
 		};
 
 		//  Stop autoplay
-		this.stop = function() {
-			this.timer = clearInterval(this.timer);
-			return this;
+		_.stop = function() {
+			_.timer = clearInterval(_.timer);
+			return _;
 		};
 
 		//  Navigation
-		this.next = function() {
-			return this.stop().to(this.i + 1);
+		_.next = function() {
+			return _.stop().to(_.i + 1);
 		};
 
-		this.prev = function() {
-			return this.stop().to(this.i - 1);
+		_.prev = function() {
+			return _.stop().to(_.i - 1);
 		};
 
-		this.nav = function(name, html) {
+		_.nav = function(name, html) {
 			if (name == _dot) {
 				html = '<ol class="dots">';
 					$.each(this.li, function(index) {
@@ -274,13 +267,13 @@
 				html += '</ol>';
 			} else {
 				html = '<div class="';
-				html = html + name + 's">' + html + name + ' prev">' + this.o.prev + '</div>' + html + name + ' next">' + this.o.next + '</div></div>';
+				html = html + name + 's">' + html + name + ' prev">' + _.o.prev + '</div>' + html + name + ' next">' + _.o.next + '</div></div>';
 			};
 
-			this.el.addClass('has-' + name + 's').append(html).find('.' + name).on('click', $.proxy(function(e) {
-				var me = $(e.target);
-				me.hasClass(_dot) ? this.stop().to(me.index()) : me.hasClass('prev') ? this.prev() : this.next();
-			}, this));
+			_.el.addClass('has-' + name + 's').append(html).find('.' + name).click(function() {
+				var me = $(this);
+				me.hasClass(_dot) ? _.stop().to(me.index()) : me.hasClass('prev') ? _.prev() : _.next();
+			});
 		};
 	};
 
